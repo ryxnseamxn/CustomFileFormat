25c0a3ca8f4a0bb7d8e55c4d58b97f4ee369d095
Ryan Seaman <ryan.l.seaman@gmail.com>
2025-01-10 22:55:06 -0600

    learning slowly

--- main.rs
+++ main.rs
@@ -1,12 +1,14 @@
 use image::{save_buffer, GenericImageView, ImageBuffer, ImageReader, Rgb};
-use std::io::Cursor; 
+use std::{io::Cursor, os::macos::raw}; 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let img = ImageReader::open("gavarni.jpg")?.decode()?;
 
     let (width, height) = img.dimensions(); 
     let raw_pixels = img.to_rgb8().into_raw(); 
-    let buffer = ImageBuffer::from_raw(width, height, raw_pixels).expect("failed to create"); 
-    print!("{:?}", img);
+    let buffer: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_raw(width, height, raw_pixels).expect("failed to create"); 
+    for pixel in buffer.iter() {
+        println!("{:?}", pixel); 
+    }
     Ok(())
 }
